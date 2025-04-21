b043ac2a7385f031021f51d6bf721d12884a767d
Araki Takashi <ellaydelgado@gmail.com>
2025-04-22 05:45:02 +0900

    update settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,10 +4,3 @@ vim.g.maplocalleader = "\\"
 local opt = vim.opt
 
 opt.tabstop = 4
-
--- Folding {{{
-vim.opt.foldmethod = "marker" -- マーカー方式の折りたたみを使用
-vim.opt.foldmarker = "{{{,}}}" -- 折りたたみマーカーを設定
-vim.opt.foldenable = true -- 折りたたみを有効化
-vim.opt.foldlevelstart = 0 -- ファイルを開いたときに全ての折りたたみを閉じる
--- }}}
