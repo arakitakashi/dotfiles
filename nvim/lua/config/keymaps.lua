288b856e564054f38189a7432c7c085b7c6a0051
Araki Takashi <ellaydelgado@gmail.com>
2025-04-30 04:55:44 +0900

    explorerの幅調整

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,8 +6,8 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("t", "jk", "<C-\\><C-n>")
 
 -- Edit and source keymap.lua
-keymap("n", "<Leader>ev", ":vsplit ~/.config/nvim/init.lua", opts)
-keymap("n", "<Leader>sv", ":luafile ~/.config/nvim/init.lua", opts)
+keymap("n", "<Leader>km", ":vsplit ~/.config/nvim/lua/config/keymaps.lua<CR>", opts)
+keymap("n", "<Leader>sk", ":luafile ~/.config/nvim/lua/config/keymaps.lua<CR>", opts)
 
 -- Edit
 keymap("n", "-", "ddp", opts)
@@ -15,6 +15,7 @@ keymap("n", "+", "ddkP", opts)
 keymap("n", "/", "/\\v", opts)
 keymap("v", "<leader>c", [["*y]], opts)
 keymap("n", "<leader>ac", [[gg"*yG]], opts)
+keymap("i", "<C-f>", "<Right>")
 
 -- Surround word or selection with quotes/brackets
 local surround_map = {
@@ -39,6 +40,31 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "javascript",
   callback = function()
     vim.keymap.set("n", "<localleader>c", "I//<ESC>", { buffer = true })
+  end,
+})
+--- }}}
+
+--- Haskell {{{
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "haskell",
+  callback = function()
+    vim.keymap.set("i", "<C-u>", function()
+      local line = vim.api.nvim_get_current_line()
+      local func_name = string.match(line, "^%s*(%w+)")
+
+      if not func_name then
+        vim.notify("関数名が見つかりません", vim.log.levels.ERROR)
+        return
+      end
+
+      local current_line = vim.api.nvim_win_get_cursor(0)[1]
+      vim.api.nvim_buf_set_lines(0, current_line - 1, current_line, false, {
+        func_name .. " :: ",
+        func_name .. " ",
+      })
+
+      vim.api.nvim_win_set_cursor(0, { current_line, #func_name + 4 })
+    end, { buffer = true, desc = "Haskell function signature" })
   end,
 })
 --- }}}
---
+++ nvim/lua/plugins/explorer.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      layout = {
+        width = 30,
+        min_width = 30,
+      },
+      layouts = {
+        sidebar = {
+          layout = {
+            width = 30,
+            min_width = 30,
+          },
+        },
+      },
+    },
+    explorer = {
+      width = 30,
+      min_width = 30,
+    },
+  },
+}
